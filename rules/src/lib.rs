1fd0effc63ddd10ac97ae07fbfe5e016ab7374cd
Jamie Friel <jamiefriel@googlemail.com>
2021-02-14 16:28:36 +0000

    set up silly test to make sure it was working

--- rules/src/lib.rs
+++ rules/src/lib.rs
@@ -1,3 +1,5 @@
-pub fn hello(){
-    println!("Hello")
+pub fn adds_hopefully(x:i32, y:i32)-> i32{
+    // Making sure tests work!
+    x+y
 }
+
--- rules/tests/tests.rs
+++ rules/tests/tests.rs
@@ -1,7 +1,12 @@
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+
+#[test]
+fn it_works() {
+    assert_eq!(2 + 2, 4);
+}
+
+
+#[test]
+fn import_test(){
+    assert_eq!(rules::adds_hopefully(1,1), 2)
 }
