6dc90bd8b8e7a7e3f2c3a485764c703bd909f899
Jamie Friel <jamiefriel@googlemail.com>
2021-05-09 00:42:50 +0100

    add notation to coord functino

--- rules/src/lib.rs
+++ rules/src/lib.rs
@@ -329,6 +329,16 @@ impl Board {
         self.square[square_2_pos.0][square_2_pos.1].possible_moves = vec![];
 
         self.square[square_1_pos.0][square_1_pos.1].set_empty();
+    }
+
+    pub fn notation_to_coord(str_move: String)->[u8;2]{
+
+        if str_move.chars().count() == 2{
+            let x1: u8 = 97 - str_move.as_bytes()[0];
+            let y1: u8 = 49 - str_move.as_bytes()[1];
+            return [x1, y1];
+        }
+       return [9, 9]; 
 
     }
 }
