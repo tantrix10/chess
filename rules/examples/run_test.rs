65f67843b445afe8353542a77b0f44e9e672210b
Jamie Friel <jamiefriel@googlemail.com>
2021-05-22 23:41:17 +0100

    remove print statements from test

--- rules/examples/run_test.rs
+++ rules/examples/run_test.rs
@@ -5,13 +5,15 @@ fn main(){
     // Set up the board to the standard starting position
     board.set();
     // Print the board to terminal
-    board.print_board();
+    // board.print_board();
     // Play the move 1.e4
     board.make_move((6,4), (4,4));
     // Print the board again to show new move
-    board.print_board();
+    // board.print_board();
     // Silly move of Rh8xe4 to show piece transfer working
     // This will break once legality is enforced
     board.make_move((0,7), (4,4));
-    board.print_board();
+    // board.print_board();
+    assert_eq!(board.square[4][4].piece, rules::Piece::Rook);
+    assert_eq!(board.square[6][4].piece, rules::Piece::Empty)
 }
