806465c055f30089f4e6e99a1f505159fd687b72
Jamie Friel <jamiefriel@googlemail.com>
2021-05-30 01:20:36 +0100

    Making some silly changes that were needed to tests.
    Removed old functions that are now obsilete.
    Added 'testing' to just show the valid moves

--- rules/examples/run_test.rs
+++ rules/examples/run_test.rs
@@ -13,7 +13,23 @@ fn main(){
     // Silly move of Rh8xe4 to show piece transfer working
     // This will break once legality is enforced
     board.make_move((0,7), (4,4));
-    // board.print_board();
+    board.print_board(None);
+    board.check_moves(true);
+    board.print_pos((4 as usize,4 as usize));
+    board.print_board(Some([4 as usize,4 as usize]));
     assert_eq!(board.square[4][4].piece, rules::Piece::Rook);
-    assert_eq!(board.square[6][4].piece, rules::Piece::Empty)
+    assert_eq!(board.square[6][4].piece, rules::Piece::Empty);
+
+    board.make_move((7,2), (4,4));
+    board.print_board(None);
+    board.check_moves(true);
+    board.print_pos((4 as usize,4 as usize));
+    board.print_board(Some([4 as usize,4 as usize]));
+
+    board.make_move((0,3), (4,4));
+    board.print_board(None);
+    board.check_moves(true);
+    board.print_pos((4 as usize,4 as usize));
+    board.print_board(Some([4 as usize,4 as usize]));
+
 }
--- rules/tests/tests.rs
+++ rules/tests/tests.rs
@@ -1,14 +1,6 @@
 // #[cfg(test)]
 
-use rules;
-
 #[test]
 fn it_works() {
     assert_eq!(2 + 2, 4);
-}
-
-
-#[test]
-fn import_test(){
-    assert_eq!(rules::adds_hopefully(1,1), 2)
 }
